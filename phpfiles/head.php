dd3ac3d94464637c96d2ba3f30b62f06cf83fd4a
gjossep <gjossep@Gjosses-MacBook-Pro-2.local>
2015-06-06 15:12:04 +0200

    Updated Head.php Core

--- phpfiles/head.php
+++ phpfiles/head.php
@@ -1,11 +1,3 @@
-<head>
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>MineTribe</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet"/>
@@ -34,5 +26,3 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
-   </head>
