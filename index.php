1e8cb535142cf7f213df4f675169abbe063b48a5
otto1996 <Otto@Ottos-iMac.local>
2015-06-07 16:01:05 +0200

    Add correct link to leaderboard

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                         <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
                     </span>
                     <div class="icons">
-                     <a href="tribeleader.php"> <img src="img/tribesicon.png" width="273" height="181" alt="tribe"> </a>
+                     <a href="tribe.php"> <img src="img/tribesicon.png" width="273" height="181" alt="tribe"> </a>
                     </div>
                   <h4 class="service-heading">Tribes</h4>
                     <p class="text-muted">Find out what other tribes are out there! See how you are doing with your tribe compared to other tribes, or if you are looking for a tribe then check if anyone is recruiting!</p>
@@ -44,7 +44,7 @@
                         <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
                     </span>
                     <div class="icons">
-                    <a href="leader.php"><img src="img/leadericons.png" width="257" height="181" alt="tribe"></a> 
+                    <a href="tribeleader.php"><img src="img/leadericons.png" width="257" height="181" alt="tribe"></a> 
                     </div>
                   <h4 class="service-heading">Leaderboards</h4>
                     <p class="text-muted">Look at the tribe leaderboards, is your tribe able to compete with the other big tribes out there?</p>
