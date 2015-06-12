9bf231ba9607ba6114aaaaf3b0495a941499b2d3
otto1996 <Otto@Ottos-iMac.local>
2015-06-12 12:54:55 +0200

    small changes

--- pricetable.php
+++ pricetable.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
@@ -12,9 +12,7 @@
 
     <!-- Custom styles for this template -->
     <link href="css/pricetable/main.css" rel="stylesheet">
-    <style type="text/css">
-.bottom {
-}
+<style type="text/css">
 .info {
 	color: #000;
 	background-color: #FFF;
@@ -28,15 +26,31 @@
 	line-height: 10px;
 	padding-top: 5px;
 }
+
+.container h1 {
+	background: #E74C3C;
+	padding-left: 20px;
+	padding-right: 20px;
+	padding-top: 10px;
+	color:#FFF
+}
+
+.container {
+	background: #E1E1E1;
+}
+
+.page {
+	background:url(img/backgroundpricetable.png);
+}
 </style>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
       <script src="js/respond.min.js"></script>
       <![endif]-->
-    </head>
-
+</head>
+<div class="page">
     <body>
 <div class="container">
       <h1 class="page-header">Join MineTribe</h1>
@@ -63,7 +77,7 @@
         <li> <strong>Statistics</strong> Graphs, track your stats over time </li>
         <li> <strong>Customizable Tribe Page</strong> </li>
         <li> <strong>Comment section on Tribe Page</strong> </li>
-        <li> <strong>Tribe Ranking</strong> Add levels of ranking. </li>
+        <li> <strong>Tribe Ranking</strong> Add levels of ranking </li>
         <li> <strong>Tribe Members</strong> Unlimited </li>
         <li> <strong>Remove Ads</strong> </li>
         <li class="plan-action"> <a href="#" class="btn btn-danger btn-lg">Register</a> </li>
@@ -88,32 +102,34 @@
       <center>
     Messaging
   </center>
-      <h3>
+      </h3>
       <p>Messaging is open to all users, however  Oulad (Clan-Members) or Khanewar (Family-Members) can create group chat. Group chats are great if you are just getting online and you want to quickly get everyone to the same server. Group messaging is very convenient for tribes who want to plan events or get togethers.</p>
       <hr>
       <h3>
       <center>
     Statistics/Graphs
   </center>
-      <h3>
+      </h3>
       <p>Statistics are open to all users, users can check how they are doing in the ranking. All users can also look at their fun statistics located on their profile. However Oulad and Khanewar receive an extra tab that shows them their improvement over time. The data starts recording from the day you start paying. You will be able to see how your stats are improving over the week, and how much you went up in which stats on certain days. This is a very usefull tool for more experienced and competitive players, who want to play on servers without dying or getting a lot of pvp kills. Get your servers to download the MineTribe Statistics plugin so that your stats are accurately portrayed.</p>
       <hr>
       <h3>
       <center>
     Customizable Tribe Page
   </center>
-      <h3>
+      </h3>
       <p>Tribe pages customizability for Tribesmen is limited, they can change their cover photo and it displays the users they have and how they are doing as a tribe. Their Tribe does compete in the Tribe leaderboard but it limited to 5 users. However Oulad and Khanewar members can add a Comment Section that anyone can comment on. They can embed videos into their page, if they want to advertise their Tribe to skilled players. They can have ranking within the Tribe, add up to 3 levels of ranks in your Tribe like: Moderator, TribeMember, New-TribeMember. The highest ranking gets the ability to also invite people to the Tribe, however only Oulad's and Khanewars's can be ranked up to that level.</p>
       <hr>
       <h3>
       <center>
     Tribe Members
   </center>
-      <h3>
+      </h3>
       <p>Tribesmen can only have up to 5 tribe members. This isn't a big deal for everyone because 5 members is a nice number if you are making one with your friends from school. However if you are making a Tribe with players you are meeting in Minecraft then you will want more users. It is also difficult to reach high in the Tribe leaderboardwith less players unless you have very experiences players. Khanewar can also join multiple tribes, up to 3. We added this feature because there are users out there who like to lead and we don't want them to be limited to helping lead only 1 Tribe.</p>
+      <hr>
     </div>
 <div class="bottom">
       <?php require('phpfiles/footer.php'); ?>
+    </div>
     </div>
 </body>
 </html>
